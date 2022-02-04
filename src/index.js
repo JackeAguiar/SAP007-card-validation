8282d1aba25a215cdeb6295d25c1917ae3eceae5
JackeAguiar <jackelineaguiar000@gmail.com>
2022-02-04 15:44:13 -0300

    testando SSH--allow-empty

--- src/index.js
+++ src/index.js
@@ -1,19 +1,73 @@
-import validator from "./validator";
+ import validator from "./validator.js";
 
-document.getElementById("btn").addEventListener("click",validar);
+document.getElementById("btn").addEventListener("click", validatorCard);
 
+var recuperarInput = document.getElementById("numeroDoCartao"); //aqui eu peguei o input pelo id
+var digitos = recuperarInput.value; //recuperei o valor do input
+var valid = validator.isValid(digitos)
+function validatorCard() {
 
-function validar() {
-    var recuperarInput = document.getElementById("numeroDoCartao"); //aqui eu peguei o input pelo id
-    var digitos = recuperarInput.value; //recuperei o valor do input
-    console.log(digitos);
-    
+    // var recuperarInput = document.getElementById("numeroDoCartao"); //aqui eu peguei o input pelo id
+    // var digitos = recuperarInput.value; //recuperei o valor do input
+    // var valid=validator.isValid(digitos)
+    //  console.log(digitos);
     recuperarInput.value = digitos.slice(0, 16); //aqui eu estou limitando a quantidade de numeros que vai entrar no input
-    if (digitos.length != 16){ 
-        return alert("Seu cartão tem que ter 16 digitos para a validação");}
-        console.log("Validar Cartão", digitos);
-    
-        var marray = digitos.split(""); //aqui transformo a String em array
-        marray.reverse()
-        console.log(marray);
+    if (digitos.length <= 4) {
+        return alert("Seu cartão precisa ter mais de 4 digitos para a validação")
+
     }
+}
+ function e(valid) {
+   (e)prefal 
+    if (valid) {
+        document.getElementById("card").innerHTML = "Valido"
+    }
+    else {
+        document.getElementById("card").innerHTML = "Inválido"
+    }
+
+     
+ }
+
+    // var arr= arr.reverse(); 
+    // var valid=validator.isValid(digitos)
+    //  console.log(arr);
+    // var valid = validator.isValid(digitos);
+    //  console.log(valid)
+    
+    // if (valid){
+    //     document.getElementById("card").innerHTML = "Valido"
+    // }
+    // else {
+    //     document.getElementById("card").innerHTML = "Inválido"
+    // }
+
+
+
+
+
+
+
+        // var arr = arr.reverse() 
+    
+        //  var arr = digitos.split("");
+
+ 
+     //aqui transformo a String em array
+//         arr.reverse()
+//         console.log(arr);
+//  var valid = validator.isValid(digitos);
+
+//     if (valid){
+//         document.getElementById("card").innerHTML = "Valido"
+//     }
+//     else {
+//         document.getElementById("card").innerHTML = "Inválido"
+//     }
+
+    
+
+
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,6 +1,34 @@
-const validator = {
-    // ...
-  };
-  
-  export default validator;
+const validator = {  
+    isValid:function (digitos) {
+        var arr = digitos.split("").reverse();
+        console.log(arr);
+     for (var numbers = 0; numbers< arr.lenght; numbers++){
+         arr[numbers] = parseInt(arr[numbers])
+         console.log(numbers)
+     }
+    var numrever = arr
+    console.log(numrever)
+    for (var i = 0; i < numrever.lenght; i++){
+
+        if(i % 2 !==0){
+            var double = numrever[i] *2;
+           numrever[i] = double;
+           if (numrever[i] > 9){
+               numrever[i] -=9;
+           }
+        }
+    }
+    // console.log("loop", numrever);
+
+   for (var i in numrever) {
+       sumNum += (numrever[i]);
+   }
+    // console.log("soma",sumNum);
+    return sumNum % 10 ===0;
+}
+   
+
+
+};
+export default validator;
   
